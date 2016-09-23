45a8d31112a2751bb175adb2420be8d8e9a055a8
jasyiu <jas903@yahoo.com.hk>
2016-09-23 21:29:15 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,6 @@
-console.log('This would be the main JS file.');
+jQuery(document).ready(function($) {  
+// site preloader -- also uncomment the div in the header and the css style for #preloader
+$(window).load(function(){
+    $('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+});
